acac79778a0cec6410a6ff489fc3c476a40f83d3
agustinabecgut <agustina.b.becerra@gmail.com>
2022-10-06 21:17:52 -0300

    agregado de funciones de dubujar horca, agregar y guardar palabra. También se agregaron mensajes de fin del juego

--- canvas.js
+++ canvas.js
@@ -12,6 +12,116 @@ function dibujarCanvas() {
     tablero.lineTo(460, 175);
     tablero.stroke();
     tablero.closePath();
+
+    tablero.beginPath();
+    tablero.moveTo(430, 175);
+    tablero.lineTo(430, 5);
+    tablero.moveTo(430, 5);
+    tablero.lineTo(490, 5);
+    tablero.moveTo(490, 5);
+    tablero.lineTo(490, 20);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarCabeza() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#fceba6";
+    tablero.strokeStyle = "#bc4a48";
+
+    tablero.beginPath();
+    tablero.arc(490, 40, 20, 0, 2*Math.PI);
+    tablero.fill();
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarTronco() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#fceba6";
+    tablero.strokeStyle = "#bc4a48";
+
+    tablero.beginPath();
+    tablero.moveTo(490, 60);
+    tablero.lineTo(490, 120);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarPiernaIzq() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#fceba6";
+    tablero.strokeStyle = "#bc4a48";
+
+    tablero.beginPath();
+    tablero.moveTo(490, 120);
+    tablero.lineTo(475, 140);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarPiernaDer() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#fceba6";
+    tablero.strokeStyle = "#bc4a48";
+
+    tablero.beginPath();
+    tablero.moveTo(490, 120);
+    tablero.lineTo(505, 140);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarBrazoIzq() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#fceba6";
+    tablero.strokeStyle = "#bc4a48";
+
+    tablero.beginPath();
+    tablero.moveTo(490, 70);
+    tablero.lineTo(475, 90);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarBrazoDer() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#fceba6";
+    tablero.strokeStyle = "#bc4a48";
+
+    tablero.beginPath();
+    tablero.moveTo(490, 70);
+    tablero.lineTo(505, 90);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarHorca() {
+    if (errores === 5) {
+        dibujarCabeza();
+    } else if (errores === 4) {
+        dibujarTronco();
+    } else if (errores === 3) {
+        dibujarPiernaIzq();
+    } else if (errores === 2) {
+        dibujarPiernaDer();
+    } else if (errores === 1) {
+        dibujarBrazoIzq();
+    } else if (errores === 0) {
+        dibujarBrazoDer();
+    }
 }
 
 function subrayar() {
@@ -51,3 +161,4 @@ function escribirLetraIncorrecta(letra, errorsLeft) {
     tablero.fillStyle = "#bc4a48";
     tablero.fillText(letra, 100 + (40*(10-errorsLeft)), 300, 40); 
 }
+
--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let palabras = ["PECERA", "RADIANTE", "MESA", "INFUSION", "OLA", "DESACATO", "GA
 let tablero = document.getElementById("canvas").getContext("2d");
 let palabraSecreta = "";
 let letras = [];
-let errores = 8;
+let errores = 6;
+
+//onload
+function principal() {
+    document.getElementById("main-iniciar").style.display = "block";
+    document.getElementById("main-jugar").style.display = "none";
+    document.getElementById("main-agregar").style.display = "none";
+}
 
 //Palabra secreta
 function elegirPalabraSecreta() {
@@ -30,10 +37,12 @@ function comprobarLetra(key) {
 //Errores
 function agregarLetraIncorrecta() {
     errores = errores - 1;
+    console.log(errores);
 }
 
 //Iniciar juego
 function iniciarJuego() {
+    document.getElementById("main-jugar").style.display = "block";
     document.getElementById("main-iniciar").style.display = "none";
     document.getElementById("main-agregar").style.display = "none";
 
@@ -42,8 +51,8 @@ function iniciarJuego() {
     subrayar();
 
     document.onkeydown = (e) => {
-        let letra = e.key.toUpperCase();
-        
+        let letra = e.key.toUpperCase();    
+
         if (comprobarLetra(letra) && palabraSecreta.includes(letra)) {
             for(let i = 0; i < palabraSecreta.length; i++) {
                 if (palabraSecreta[i] === letra) {
@@ -53,6 +62,37 @@ function iniciarJuego() {
         } else {
             agregarLetraIncorrecta(letra);
             escribirLetraIncorrecta(letra, errores);
+            dibujarHorca();
         }
+    }
+}
+
+//Fin del juego
+//function finDelJuego() {
+//    if (errores === 0) {
+//
+//    }
+//}
+
+//Agregar palabra
+function agregarPalabra() {
+    document.getElementById("main-agregar").style.display = "block";
+    document.getElementById("main-iniciar").style.display = "none";
+    document.getElementById("main-jugar").style.display = "none";
+}
+
+//Guardar palabra agregada
+function guardarPalabra() {
+    let palabra = document.getElementById("nueva-palabra").value;
+    let longitud = palabra.length;
+
+    if (longitud < 3) {
+        alert("La palabra debe tener al menos 3 letras");
+    } else if (longitud > 8) {
+        alert("La palabra debe tener hasta 8 letras como máximo");
+    } else {
+        palabras.push(palabra.toUpperCase());
+        console.log(palabras)
+        iniciarJuego();
     }
 }
